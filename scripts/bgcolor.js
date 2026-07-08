170974a0b62a8f957354f31a3894695244d18ba4
Erik S. <eriksom764@gmail.com>
2026-07-08 11:00:54 +0200

    repositioned function bgColor

---
+++ scripts/bgcolor.js
@@ -0,0 +1,60 @@
+function bgColor(type) {
+  switch (type) {
+    case "bug":
+      classList.add(".bg_bug");
+      break;
+    case "fire":
+      classList.add(".bg_fire");
+      break;
+    case "normal":
+      classList.add(".bg_normal");
+      break;
+    case "dark":
+      classList.add(".bg_dark");
+      break;
+    case "flying":
+      classList.add(".bg_flying");
+      break;
+    case "poison":
+      classList.add(".bg_poison");
+      break;
+    case "dragon":
+      classList.add(".bg_dragon");
+      break;
+    case "ghost":
+      classList.add(".bg_ghost");
+      break;
+    case "psychic":
+      classList.add(".bg_psychic");
+      break;
+    case "electric":
+      classList.add(".bg_electric");
+      break;
+    case "grass":
+      classList.add(".bg_grass");
+      break;
+    case "rock":
+      classList.add(".bg_rock");
+      break;
+    case "fairy":
+      classList.add(".bg_fairy");
+      break;
+    case "ground":
+      classList.add(".bg_ground");
+      break;
+    case "steel":
+      classList.add(".bg_steel");
+      break;
+    case "fighting":
+      classList.add(".bg_fighting");
+      break;
+    case "ice":
+      classList.add(".bg_ice");
+      break;
+    case "water":
+      classList.add(".bg_water");
+      break;
+    default:
+      break;
+  }
+}
